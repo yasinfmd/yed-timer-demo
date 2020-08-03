d6b15e32a52d05c9714b55e462f65c2249d552ec
ydalkilic <ysndlklc1234@gmail.com>
2020-08-03 15:03:22 +0300

    demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import  YedTimer from 'yed-timer'
+import 'yed-timer/dist/index.css'
+
+const App = () => {
+
+  return <>
+    <div>
+      <b>Standart Kullanım</b>
+      <YedTimer minute={40} second={15} />
     </div>
-  );
+
+    <div className="mt20" >
+      <b>Genişlik ve Yükseklik Değerleri ile Beraber Kullanım</b>
+      <YedTimer timerWidth={500} timerHeight={250} minute={40} second={15} />
+    </div>
+    <div className="mt20" >
+      <b>Sayaç Butonlarının Özelleştirilmesiyle Beraber Kullanım</b>
+      <YedTimer resetText={"Yeniden Başlat"} startText={"Sayacı Başlat"}  stopText={"Sayacı Durdur"}  minute={40} second={15} />
+    </div>
+    <div className="mt20" >
+      <b>Dakika ve Saniye Metinlerinin Özelleştirilmesiyle Beraber Kullanım </b>
+      <YedTimer resetText={"Yeniden Başlat"} startText={"Sayacı Başlat"}  minuteText={"Dk"} secondText={"Sn"} stopText={"Sayacı Durdur"}  minute={40} second={15} />
+    </div>
+    </>
 }
 
-export default App;
+export default App
