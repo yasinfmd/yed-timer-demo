53f68421e2be13e8ffd418f95aaf1b5e2cdf7790
ydalkilic <ysndlklc1234@gmail.com>
2020-08-03 15:20:34 +0300

    Updates

---
+++ precache-manifest.c6d7b182e36b523270ca3b4a23ebde31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088758db8451340a60123a12d23b02df",
+    "url": "/yed-timer-demo/index.html"
+  },
+  {
+    "revision": "82c202975f5002d07af8",
+    "url": "/yed-timer-demo/static/css/2.d6b0cf4f.chunk.css"
+  },
+  {
+    "revision": "371b58468601bedeaf3a",
+    "url": "/yed-timer-demo/static/css/main.f7cd2ea1.chunk.css"
+  },
+  {
+    "revision": "82c202975f5002d07af8",
+    "url": "/yed-timer-demo/static/js/2.ee626653.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yed-timer-demo/static/js/2.ee626653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371b58468601bedeaf3a",
+    "url": "/yed-timer-demo/static/js/main.e8f68911.chunk.js"
+  },
+  {
+    "revision": "395b6bb3a239c35d32e6",
+    "url": "/yed-timer-demo/static/js/runtime-main.5f5c7bb8.js"
+  }
+]);
